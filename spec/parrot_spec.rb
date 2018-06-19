0047ecb7b8a9e6a715af241fc7fc77a37521d5fd
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-19 15:10:03 +0000

    automatically backed up by learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,6 +1,6 @@
 require_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
-def parrot(string = "Squawk")
+def parrot(string = "Squawk!")
   puts string
   string
 end
